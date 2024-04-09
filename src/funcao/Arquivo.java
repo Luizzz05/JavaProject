dad38f37ef823ab857fb4b6d4dfa7ece92a59305
Luiz Felipe S Santos <lfsilvasantos55@gmail.com>
2024-04-09 11:31:32 -0300

    Novas funçoes para ler arquivos

--- src/funcao/Arquivo.java
+++ src/funcao/Arquivo.java
@@ -1,32 +1,135 @@
+/**
+ * 
+ */
 package funcao;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
+import model.Product;
+
+/**
+ * 
+ */
 public class Arquivo {
 	
+	List<Product> list = new ArrayList<>();
 	public void readFile1(String arquivo) {
-		
+//		File path = new File("/home/fennder/Documents/Workspace/eclipse-workspace/JavaProject/src/tmp/" + arquivo);
 		File path = new File("/home/luiz/Documentos/eclipse-workspace/Projeto_WEB/src/tmp/" + arquivo);
 		Scanner read = null;
 		try {
 			read = new Scanner(path);
 			while(read.hasNextLine()) {
-				JOptionPane.showMessageDialog(null, read.nextLine());				
+				System.out.println(read.nextLine());
+//				JOptionPareadFile3ne.showMessageDialog(null, read.nextLine());				
 			}
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			//	e.printStackTrace();
-			JOptionPane.showMessageDialog(null, e.getMessage());
+			System.out.println(e.getMessage());
+//			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
 		finally {
 			if(read != null) {
 				read.close();
 			}
+		}
+	}
+
+	public void readFile2(String arquivo) {
+		// Variável
+//		String path = "/home/fennder/Documents/Workspace/eclipse-workspace/JavaProject/src/tmp/" + arquivo;
+		File path = new File("/home/luiz/Documentos/eclipse-workspace/Projeto_WEB/src/tmp/" + arquivo);
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			String line = br.readLine();
+			while(line != null) {
+				JOptionPane.showMessageDialog(null, line);
+				line = br.readLine();
+			}
+		}
+		catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+		}
+		finally{
+			try {
+				br.close();
+				fr.close();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void readFile3(String arquivo) {
+		String path = "/home/luiz/Documentos/eclipse-workspace/Projeto_WEB/src/tmp/" + arquivo;//Simplificado
+		
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){ //Simplificado
+			String line = br.readLine(); //Simplificado
+			line = br.readLine();
+			while(line != null) { //Simplificado
+//				JOptionPane.showMessageDialog(null, line); //Simplificado
+				
+				String[] vect = line.split(",");
+				String name = vect[0];
+				Double price = Double.parseDouble(vect[1]);
+				Integer qte = Integer.parseInt(vect[2]);
+//				Double total = Double.parseDouble(vect[3]);
+				
+				Product prod = new Product(name, price, qte);
+				list.add(prod);
+				
+				line = br.readLine(); //Simplificado
+				
+			}
+//			JOptionPane.showMessageDialog(null, "Produtos");
+			System.out.println("Produtos");
+			for(Product p :list) {
+//				JOptionPane.showMessageDialog(null, p);
+				System.out.println(p);
+			}
+			
+		}
+		catch(IOException e) {
+			JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());//Simplificado
+		}
+	}
+	
+	public void writeFile(String arquivo) throws IOException{
+		String path = "/home/luiz/Documentos/eclipse-workspace/Projeto_WEB/src/tmp/" + arquivo;
+		
+//		List<Product> list = new ArrayList<>();
+		try {
+			BufferedWriter br = new BufferedWriter(new FileWriter(path, true));
+//			FileWriter fw = new FileWriter(path, true);
+//			fw.write(JOptionPane.showInputDialog(null, "Digite o produto, Descrição, Valor, Quantidade:"+"\n"));
+			br.write(JOptionPane.showInputDialog(null, "Digite o produto, Descrição: "));
+			br.write(",");
+			br.write(JOptionPane.showInputDialog(null, "Digite o produto, Valor: "));
+			br.write(",");
+			br.write(JOptionPane.showInputDialog(null, "Digite o produto, Quantidade: "));
+			
+			br.write("\n");
+			br.close();
+						
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
--- src/principal/main.java
+++ src/principal/main.java
@@ -1,38 +1,39 @@
 package principal;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 import funcao.Arquivo;
 import funcao.Funcao;
-import model.People;
 
 public class main {
 
-	public static void main(String[] args) {
-	//Declaração de variáveis
-	String mensagem, arquivo;
-	Funcao funcao = new Funcao();
-	Arquivo arq = new Arquivo();
-	
-	//Usando método de passagem de parâmetro com retorno
-//	mensagem = JOptionPane.showInputDialog("Digite o Seu nome: ");
-//	funcao.mensagem(mensagem);
-//	JOptionPane.showMessageDialog(null, funcao.mensagem(mensagem));
-	
-//	funcao.media();
-//	funcao.printPerson();
+	public static void main(String[] args) throws IOException {
+		// BLOCO - Declaração de variáveis
+		String mensagem, arquivo;
+		Funcao funcao = new Funcao();
+		Arquivo arq = new Arquivo();
+		
+		//Usando método de passagem de parâmetro com retorno
+//		mensagem = JOptionPane.showInputDialog("Digite o seu nome: ");
+//		funcao.mensagem(mensagem);
+//		JOptionPane.showMessageDialog(null, funcao.mensagem(mensagem));
+		
+//		funcao.media();
+//		funcao.printPerson();
 
-	
-	arquivo = JOptionPane.showInputDialog("Digite o caminho e o nome do arquivo. exemplo: 'ini.txt'': ");
-	arq.readFile1(arquivo);
-//	arq.readFile2(arquivo);
-//	arq.writeFile(arquivo);
-//	arq.readFile3(arquivo);
-//	
-	
-
+		arquivo = JOptionPane.showInputDialog("Digite o caminho e o nome do arquivo. exemplo: ''ini.txt'': ");
+//		arq.readFile1(arquivo);
+		System.out.println("");
+//		arq.readFile2(arquivo);
+		System.out.println("");
+		arq.writeFile(arquivo);
+		arq.readFile3(arquivo);
+		System.out.println("");
 	}
 }
 	
